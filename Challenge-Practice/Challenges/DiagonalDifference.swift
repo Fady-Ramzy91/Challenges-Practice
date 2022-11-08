af0380033cd3d2538314abdabc725cb0af297852
Fady Ramzy <fady.h@tempo.fit>
2022-11-08 16:29:55 +0200

    ADD: Add new problem diagonal difference

---
+++ Challenge-Practice/Challenges/DiagonalDifference.swift
@@ -0,0 +1,24 @@
+//
+//  DiagonalDifference.swift
+//  Challenge-Practice
+//
+//  Created by Fady Ramzy on 08/11/2022.
+//
+
+// Problem link: https://www.hackerrank.com/challenges/diagonal-difference
+
+import Foundation
+
+func diagonalDifference(arr: [[Int]]) -> Int {
+    var sum1: Int = 0
+    var sum2: Int = 0
+    var reverseCount = arr.count - 1
+
+    for index in 0..<arr.count {
+        sum1 += arr[index][index]
+        sum2 += arr[index][reverseCount]
+        reverseCount-=1
+    }
+    
+    return abs(sum1 - sum2)
+}
