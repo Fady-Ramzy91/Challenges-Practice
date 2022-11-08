21f93bddc91702b7051f973c8b73dc5de9f79a5c
Fady Ramzy <fady.h@tempo.fit>
2022-11-08 15:31:55 +0200

    ADD: Add new problem [Average Big Sum]

---
+++ Challenge-Practice/Challenges/AverageBigSum.swift
@@ -0,0 +1,14 @@
+//
+//  AverageBigSum.swift
+//  Challenge-Practice
+//
+//  Created by Fady Ramzy on 08/11/2022.
+//
+
+// Problem link: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+
+import Foundation
+
+func aVeryBigSum(ar: [Int]) -> Int {
+    return ar.reduce(0, +)
+}
